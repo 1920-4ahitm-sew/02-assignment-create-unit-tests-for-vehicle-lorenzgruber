f5c7a479bcf2c5f4d3b4db8a892dc2a6d76fcb14
gruberlorenz <regru.lg@gmail.com>
2019-10-18 12:40:54 +0200

    ADD test cases

--- src/test/java/at/htl/vehiclest/rest/VehicleEndPointIT.java
+++ src/test/java/at/htl/vehiclest/rest/VehicleEndPointIT.java
@@ -1,4 +1,63 @@
 package at.htl.vehiclest.rest;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import javax.json.JsonArray;
+import javax.json.JsonObject;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
 public class VehicleEndPointIT {
+    private Client client;
+    private WebTarget target;
+
+    @BeforeEach
+    public void initClient() {
+        this.client = ClientBuilder.newClient();
+        this.target = client.target("http://localhost:8080/Vehicle/api/vehicle");
+    }
+
+    @Test
+    public void fetchVehicle() {
+        Response response = (Response) this.target.request(MediaType.TEXT_PLAIN).get();
+        assertThat(response.getStatus(), is(200));
+        String payload = response.readEntity(String.class);
+        System.out.println("payload = " + payload);
+    }
+
+    @Test
+    public void fetchVehicleJSON() {
+        Response response = (Response) this.target.request(MediaType.APPLICATION_JSON).get();
+        JsonArray allTodos = response.readEntity(JsonArray.class);
+        JsonObject vehicle = allTodos.getJsonObject(0);
+        assertThat(vehicle.getString("brand"), equalTo("Opel 42"));
+        assertThat(vehicle.getString("type"), startsWith("Commodore"));
+    }
+
+    @Test
+    public void fetchVehicleWithId() {
+        JsonObject dedicatedVehicle = this.target
+                .path("43")
+                .request(MediaType.APPLICATION_JSON)
+                .get(JsonObject.class);
+        assertThat(dedicatedVehicle.getString("brand"),containsString("43"));
+        assertThat(dedicatedVehicle.getString("brand"),equalTo("Opel 43"));
+    }
+
+    @Test
+    public void deleteVehicle() {
+        Response deleteResponse = this.target
+                .path("42")
+                .request(MediaType.APPLICATION_JSON)
+                .delete();
+        assertThat(deleteResponse.getStatus(),is(204));
+    }
 }
